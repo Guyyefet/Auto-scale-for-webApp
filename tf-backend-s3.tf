55ba72c2b5b550c119db2226730652cc807bb55f
guy yefet <guy.yefet@gmail.com>
2022-12-19 18:05:29 +0200

    reconfigure backend

--- tf-backend-s3.tf
+++ tf-backend-s3.tf
@@ -1,28 +1,28 @@
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "terraform-store-state-in-s3-bucket"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-store-state-in-s3-bucket"
  
-#   # Prevent accidental deletion of this S3 bucket
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "enabled" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-#   bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 # resource "aws_s3_bucket_public_access_block" "public_access" {
 #   bucket                  = aws_s3_bucket.terraform_state.id
@@ -31,3 +31,14 @@
 #   ignore_public_acls      = true
 #   restrict_public_buckets = true
 # }
+
+# Dynamo DB Table for Locking TF Config
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
