49e4ff718c1325a7709ed106b7612ffbdadd8f77
guy yefet <guy.yefet@gmail.com>
2022-12-21 04:27:00 +0200

    terraform workflow

--- tf_files/main.tf
+++ tf_files/main.tf
@@ -1,8 +1,21 @@
+terraform {
+  backend "s3" {
+      # Replace this with your bucket name!
+    bucket         = "terraform-store-state-in-s3-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    profile = "devops_user"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+    # shared_credentials_file = "$HOME/.aws/credentials"
+  }
+}
+
 provider "aws" {
   profile = "devops_user"
   region  = "us-east-1"
-  access_key= AKIASUYKOXW37ZW7CSPP
-  secret_key= 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
 }
 
 terraform {
