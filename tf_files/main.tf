d62fe3bdbaeddaccc08fe001756aa92ff160e025
guy yefet <guy.yefet@gmail.com>
2022-12-22 07:15:29 +0200

    profile provider deleted

--- tf_files/main.tf
+++ tf_files/main.tf
@@ -4,7 +4,6 @@ terraform {
     bucket         = "terraform-store-state-in-s3-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    profile = "devops_user"
 
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
