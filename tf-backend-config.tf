f23cd4db3ec3b4be7505ec09c8a8130bd686359c
guy yefet <guy.yefet@gmail.com>
2023-01-04 14:44:13 +0200

    terra init try

--- tf-backend-config.tf
+++ tf-backend-config.tf
@@ -4,12 +4,12 @@ terraform {
     bucket         = "terraform-store-state-in-s3-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    profile = "devops_user"
+    # profile = "devops_user"
 
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
-    shared_credentials_file = "$HOME/.aws/credentials"
+    # shared_credentials_file = "$HOME/.aws/credentials"
   }
 }
 
