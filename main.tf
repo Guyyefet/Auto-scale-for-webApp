77404e0dc5ef7aeeb32c9a1260704baa357d490d
guy yefet <guy.yefet@gmail.com>
2023-01-04 14:04:36 +0200

    ec2 try

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-# terraform {
-#   backend "s3" {
-#       # Replace this with your bucket name!
-#     bucket         = "terraform-store-state-in-s3-bucket"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "us-east-1"
-
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "terraform-up-and-running-locks"
-#     encrypt        = true
-#     # shared_credentials_file = "$HOME/.aws/credentials"
-#   }
-# }
-
 terraform {
   required_providers {
     aws = {
