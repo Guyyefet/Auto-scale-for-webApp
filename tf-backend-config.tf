3b650b071d4688d22f63dfaecf989a6a4495fe5c
guy yefet <guy.yefet@gmail.com>
2023-01-04 13:16:33 +0200

    ec2 try

--- tf-backend-config.tf
+++ tf-backend-config.tf
@@ -1,20 +1,20 @@
-# terraform {
-#   backend "s3" {
-#       # Replace this with your bucket name!
-#     bucket         = "terraform-store-state-in-s3-bucket"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "us-east-1"
-#     profile = "devops_user"
-
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "terraform-up-and-running-locks"
-#     encrypt        = true
-#     shared_credentials_file = "$HOME/.aws/credentials"
-#   }
-# }
-
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+      # Replace this with your bucket name!
+    bucket         = "terraform-store-state-in-s3-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    profile = "devops_user"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+    shared_credentials_file = "$HOME/.aws/credentials"
   }
 }
+
+# terraform {
+#   backend "local" {
+#     path = "terraform.tfstate"
+#   }
+# }
