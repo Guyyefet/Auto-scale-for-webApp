ab4ae8c89cd8a57a6e95ea8c0203cec3b74368a3
guy yefet <guy.yefet@gmail.com>
2023-01-04 13:51:42 +0200

    ec2 try

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example 1" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
