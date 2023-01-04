da9fd3277ff256a31385a03ed3cd6bef1eb5e490
guy yefet <guy.yefet@gmail.com>
2023-01-04 13:11:31 +0200

    change backend

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ provider "aws" {
 #   }
 # }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "example" {
+#   ami           = "ami-2757f631"
+#   instance_type = "t2.micro"
+# }
