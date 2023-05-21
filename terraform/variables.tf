39a4ded361673a1200bdd58efd8a43977327dcdf
guy yefet <guy.yefet@gmail.com>
2023-05-21 23:12:58 +0300

    adding variables

---
+++ terraform/variables.tf
@@ -0,0 +1,74 @@
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "instance type"
+}
+
+# variable "instance_tags" {
+#   type = object({
+#     Name = string
+
+#   })
+# }
+
+
+
+# variable "vpc_id" {
+#   type = string
+#   default = data.aws_vpc.dev-vpc.id
+#   description = "vpc_id"
+# }
+
+variable "regions" {
+  type = list(string)
+  description = "list of aws regions"
+}
+
+variable "availability_zones" {
+  type = list(string)
+  description = "availability zones"
+}
+
+variable "private_subnets" {
+  type = list(string)
+  description = "private subnets cidr blocks"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  description = "public subnets cidr blocks"
+}
+
+variable "ec2_traffic_port" {
+   type = numeric
+   default = 8080
+}
+
+variable "health_check_port" {
+   type = numeric
+   default = 8081
+}
+
+variable "ingress_alb_port" {
+  type = numeric
+  default = 80
+}
+
+variable "ec2_security_group_id" {
+  type = string
+}
+
+variable "network_protocol" {
+  type = string
+  default = "tcp"
+}
+
+variable "ingress" {
+  type = string
+  default = "ingress"
+}
+
+variable "egress" {
+  type = string
+  default = "egress"
+}
